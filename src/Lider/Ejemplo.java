1aff5dc6c3b33c230d431b3ab7efc7278480c90a
lalex <lalex@gmail.com>
2022-11-10 10:13:04 -0600

    alex

--- src/Lider/Ejemplo.java
+++ src/Lider/Ejemplo.java
@@ -12,9 +12,7 @@ public class Ejemplo extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -28,9 +26,7 @@ public class Ejemplo extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public Ejemplo() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
